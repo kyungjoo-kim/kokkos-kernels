15e5696c2bd4ec6e9a3d9a98c28efc5410df3e3b
Kyungjoo Kim <kyukim@sandia.gov>
2019-04-11 10:36:06 -0600

    KokkosBatched - fix typos

--- src/batched/KokkosBatched_Eigendecomposition_Serial_Internal.hpp
+++ src/batched/KokkosBatched_Eigendecomposition_Serial_Internal.hpp
@@ -87,7 +87,7 @@ namespace KokkosBatched {
           const auto matrix_layout = ( as1 == 1 ? LAPACK_ROW_MAJOR : LAPACK_COL_MAJOR );            
           LAPACKE_dgehrd(matrix_layout, m, 1, m, A, m, t);
             
-          SerialCopyInternal::invoke(m, m, QZ, qzs0, qzs1);          
+          SerialCopyInternal::invoke(m, m, A, as0, as1, QZ, qzs0, qzs1);          
           LAPACKE_dorghr(matrix_layout, m, 1, m, QZ, m, t);
         } else { /// for arbitrary strides, there is no choice to use tpls
           real_type *ww = w_now; w_now += m; wlen_now -= m;
